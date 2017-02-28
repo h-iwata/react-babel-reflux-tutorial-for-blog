af00d4f883c42d3163886fd1b2714ec7eeb979fe
h-iwata <mod_@hotmail.co.jp>
2017-02-28 09:54:35 +0900

    add redux sample

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,25 @@
 'use strict';
 
-import React         from 'react';
 import initParser    from './initParser.js';
 
 initParser();
+
+import ShowCounterComponent from './samples/component/ShowCounterComponent';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const Store = createStore((state = {counter: 0}, action) => {
+    switch(action.type){
+        case 'INC_COUNTER':
+            return Object.assign({}, state, {
+                counter: state.counter + action.num
+            });
+        default:
+            return state;
+    };
+});
+
+ReactDOM.render(<Provider store={Store}><ShowCounterComponent /></Provider>, document.querySelector('#mount'));
--- app/js/initParser.js
+++ app/js/initParser.js
@@ -4,12 +4,14 @@ import parser from './common/utils/domParser.js';
 
 import HelloWorldComponet from './samples/component/HelloWorldComponent';
 import ShowHelloWorldCounterButtonComponent from './samples/component/ShowHelloWorldCounterButtonComponent';
+import ShowCounterComponent from './samples/component/ShowCounterComponent';
 
 export default function ()
 {
   parser.register({
     'HelloWorldComponet': HelloWorldComponet,
     'ShowHelloWorldCounterButtonComponent': ShowHelloWorldCounterButtonComponent,
+    //'ShowCounterComponent': ShowCounterComponent,
   });
   parser.parse(document.getElementsByTagName("BODY")[0]);
 }
